b77136a47cc9914f39fc549572c46d6199c5f01f
Mehedi Alam Nahi <mdmehedialamnahi@gmail.com>
2026-06-20 02:11:26 +0600

    Post Request to Add job implemented

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,14 @@ import AddJobPage from './pages/AddJobPage';
 
 
 function App() {
-  const addJob=(newJob)=> {
-  console.log(newJob);
+  const addJob=async (newJob)=> {
+  const res = await fetch ('/api/jobs',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newJob)
+  })
 };
 
 const router = createBrowserRouter(
