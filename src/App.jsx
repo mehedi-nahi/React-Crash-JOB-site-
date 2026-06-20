a94879ecce86ada2c31f091fb35c4ae1666895f5
Md. Mehedi Alam Nahi <nahimehedi@gmail.com>
2026-06-20 12:34:06 +0600

    Job Edit/Delete implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage , { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 
 
@@ -25,18 +26,40 @@ function App() {
   })
 };
 
-const router = createBrowserRouter(
-  createRoutesFromElements (
-  <Route path='/' element = {<MainLayout />}>
-    <Route index element={ <HomePage /> } /> 
-    <Route path='/jobs' element ={<JobsPage />} />
-    <Route path = '/add-job' element ={<AddJobPage addJobSubmit={addJob} />} />
-    <Route path= '/jobs/:id' element ={<JobPage />} loader={jobLoader} />
-    <Route path='*' element ={ <NotFoundPage /> } />
-  </Route>
-  )
-);
-  return < RouterProvider router={router} />;
+//Delete Job
+const deleteJob= async (id) => {
+  const res = await fetch(`/api/jobs/${id}`, {
+    method: 'DELETE'
+  });
+  return res;
+}
+
+//Update Job
+const updateJob = async(job) =>{
+  const res = await fetch(`/api/jobs/${job.id}`,{
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(job)
+  })
 };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements (
+    <Route path='/' element = {<MainLayout />}>
+      <Route index element={ <HomePage /> } /> 
+      <Route path='/jobs' element ={<JobsPage />} />
+      <Route path = '/add-job' element ={<AddJobPage addJobSubmit={addJob} />} />
+      <Route path ='/jobs/:id' element ={<JobPage deleteJob={deleteJob}/>} loader={jobLoader} />
+      <Route path ='/edit-job/:id' element ={<EditJobPage updateJobSubmit = {updateJob}/>} loader={jobLoader} />
+      
+      <Route path='*' element ={ <NotFoundPage /> } />
+    </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+}
 
 export default App 
--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,44 +1,46 @@
-import React from 'react'
-import {useState} from "react";
-import {useNavigate} from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 
 
 const AddJobPage = ({addJobSubmit}) => {
-  const navigate= useNavigate();
-    const [title,setTitle]=useState('');
-    const [type,setType] = useState('Fulltime');
-    const [location,setLocation]= useState('');
-    const [description, setDescription] = useState('');
-    const [salary,setSalary]=useState('Under $50K');
-    const [companyName,setCompanyName]=useState('');
-    const [companyDescription, setCompanyDescription]=useState('');
-    const [contactEmail, setContactEmail]=useState('');
-    const [contactPhone, setContactPhone]=useState('');
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [type, setType] = useState('Full-Time');
+  const [location, setLocation] = useState('');
+  const [description, setDescription] = useState('');
+  const [salary, setSalary] = useState('Under $50K');
+  const [companyName, setCompanyName] = useState('');
+  const [companyDescription, setCompanyDescription] = useState('');
+  const [contactEmail, setContactEmail] = useState('');
+  const [contactPhone, setContactPhone] = useState('');
 
-    const submitForm = (e) => {
-        e.preventDefault();
+  const submitForm = (e) => {
+    e.preventDefault();
 
-        const newJob={
-            title,
-            type,
-            location,
-            description,
-            salary,
-            company: {
-                name: companyName,
-                description: companyDescription,
-                contactEmail,
-                contactPhone
-            }
-        };
-        addJobSubmit(newJob);
+    const newJob = {
+      title,
+      type,
+      location,
+      description,
+      salary,
+      company: {
+        name: companyName,
+        description: companyDescription,
+        contactEmail,
+        contactPhone,
+      },
+    };
+    addJobSubmit(newJob);
 
-        return navigate('/jobs');
-    }
+    toast.success('Job Added Successfully');
+
+    return navigate('/jobs');
+  };
 
   return (
-     <section className="bg-indigo-50">
+    <section className='bg-indigo-50'>
       <div className="container m-auto max-w-2xl py-24">
         <div
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
@@ -219,7 +221,7 @@ const AddJobPage = ({addJobSubmit}) => {
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default AddJobPage
